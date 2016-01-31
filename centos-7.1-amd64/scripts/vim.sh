d7eb46963e73e16f3219d1c547bf5d8b8158e32c
Nick Chappell <nick@intronic.org>
2016-01-30 20:45:04 -0800

    Remove the erroneous extra | bash.

--- centos-7.1-amd64/scripts/vim.sh
+++ centos-7.1-amd64/scripts/vim.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -x
 
 #Install Ruby, Ruby gems and rake
-yum -y install ruby rubygems
-gem install rake
+yum -y install ruby rubygems > /dev/null
+gem install rake > /dev/null
 
 #Install Janus for the 'vagrant' user (cd to vagrant's homedir first so we're in the right spot for the script to run)
 echo "Installing Vim Janus for the 'vagrant' user..."
-su - vagrant -c 'cd /home/vagrant; curl -Lo- https://raw.githubusercontent.com/carlhuda/janus/master/bootstrap.sh | bash | bash'
+su - vagrant -c 'cd /home/vagrant; curl -Lo- https://raw.githubusercontent.com/carlhuda/janus/master/bootstrap.sh | bash'
 
 chown -R vagrant:vagrant /home/vagrant/
 
 #Install Janus for the 'root' user (cd to root's homedir first so we're in the right spot for the script to run)
 echo "Installing Vim Janus for the 'root' user..."
-su - root -c 'cd /root; curl -Lo- https://raw.githubusercontent.com/carlhuda/janus/master/bootstrap.sh | bash | bash'
+su - root -c 'cd /root; curl -Lo- https://raw.githubusercontent.com/carlhuda/janus/master/bootstrap.sh | bash'
 
 #Remove the janus git repo history to save sapce. We want the current checkout of the files but not the whole repo history
 echo "Removing git repo histories for Janus to save space..."
