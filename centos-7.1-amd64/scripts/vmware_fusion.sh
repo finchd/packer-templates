3b654daf85b9637a18e86d02c30e5b139e9d96ca
Nick Chappell <nick@intronic.org>
2016-01-30 20:44:32 -0800

    Force the installation of the VMware VM tools.

--- centos-7.1-amd64/scripts/vmware_fusion.sh
+++ centos-7.1-amd64/scripts/vmware_fusion.sh
@@ -6,7 +6,8 @@ mkdir /tmp/vmfusion
 mkdir /tmp/vmfusion-archive
 mount -o loop /home/vagrant/linux.iso /tmp/vmfusion
 tar xzf /tmp/vmfusion/VMwareTools-*.tar.gz -C /tmp/vmfusion-archive
-/tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
+#/tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --default
+/tmp/vmfusion-archive/vmware-tools-distrib/vmware-install.pl --force-install;
 umount /tmp/vmfusion
 rm -rf  /tmp/vmfusion
 rm -rf  /tmp/vmfusion-archive
